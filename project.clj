30b8cc0c01386cd60e8a506c3e81687e58a77af3
Aphyr <aphyr@aphyr.com>
2015-11-23 20:59:52 -0800

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gnuplot "0.1.0"
+(defproject gnuplot "0.1.1"
   :description "A Clojure interface to Gnuplot"
   :url "http://github.com/aphyr/gnuplot"
   :license {:name "Eclipse Public License"
